51d646c255abf5ac081f45be089ee12c593a34dc
Muhammad-Haram <haramh643@gmail.com>
2025-02-28 16:46:33 +0500

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const connectToDb = require("./connect");
 const urlRoutes = require("./routes/url.routes");
@@ -7,8 +8,16 @@ const app = express();
 const PORT = process.env.PORT;
 
 connectToDb();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
 app.use("/url", urlRoutes);
+
+app.get("/", async (req, res) => {
+
+})
 
 app.get("/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
